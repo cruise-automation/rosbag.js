67fc01d5ab00592f8f3307d870d1a4cfd759f19f
Brian M. Carlson <brian.m.carlson@gmail.com>
2018-12-21 18:46:03 -0600

    Set global object in worker

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+// @flow
 // Copyright (c) 2018-present, GM Cruise LLC
 
 // This source code is licensed under the Apache License, Version 2.0,
@@ -7,7 +8,7 @@
 const path = require("path");
 const nodeExternals = require("webpack-node-externals");
 
-const target = process.env.ROSBAG_TARGET;
+const target = process.env.ROSBAG_TARGET || "";
 
 module.exports = {
   entry: `./src/${target}/index.js`,
@@ -28,6 +29,7 @@ module.exports = {
     path: path.resolve(__dirname, `dist/${target}`),
     library: "rosbag",
     libraryTarget: "umd",
+    globalObject: "typeof self !== 'undefined' ? self : this",
   },
   target,
   externals: target === "node" ? [nodeExternals()] : undefined,
